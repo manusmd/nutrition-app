721aa8a1ffa19213cce7a7f3c9d763cde1f4b68b
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-08 10:03:11 +0200

    Initial commit

---
+++ src/components/tracking-card/tracking-card.stories.js
@@ -0,0 +1,9 @@
+import "./tracking-card.css";
+import tabs from "./tracking-card.html";
+
+export default {
+  title: "Components/tracking-card",
+  parameters: { layout: "centered" },
+};
+
+export const favorites = () => tracking-card;
