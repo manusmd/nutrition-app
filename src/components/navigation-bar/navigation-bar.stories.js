b54c3da191047eaad3b51fc1fc2e321b5c8ba846
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-08 14:49:05 +0200

    Add html structure

---
+++ src/components/navigation-bar/navigation-bar.stories.js
@@ -0,0 +1,9 @@
+import "./navigation-bar.css";
+import navigation from "./navigation-bar.html";
+
+export default {
+  title: "Components/Navigation Bar",
+  parameters: { layout: "centered" },
+};
+
+export const navigationB = () => navigation;
