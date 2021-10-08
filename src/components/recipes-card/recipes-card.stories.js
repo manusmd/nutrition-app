9d37522aafdea9ff894cca77f8b119c7758da334
“manusmd” <manuel.schmid@js-tut.de>
2021-10-08 13:39:24 +0200

    Initial commit

---
+++ src/components/recipes-card/recipes-card.stories.js
@@ -0,0 +1,9 @@
+import "./recipes-card.css";
+import recipes from "./recipes-card.html";
+
+export default {
+  title: "Components/Recipes Card",
+  parameters: { layout: "centered" },
+};
+
+export const recipesC = () => recipes;
